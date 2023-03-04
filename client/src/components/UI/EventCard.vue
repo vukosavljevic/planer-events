bf035f1a262b46779e30c49f325cf540c7258815
vukosavljevic <domagojv2005@gmail.com>
2023-03-04 19:41:22 +0100

    feat: new events add

---
+++ client/src/components/UI/EventCard.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>{{ title }} - {{ date }}</h1>
+        <p>{{ description }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['title', 'description', 'date']
+}
+</script>
+<style scoped>
+div{
+    margin-top:5rem;
+}
+</style>
--- client/src/components/pages/LandingPage.vue
+++ client/src/components/pages/LandingPage.vue
@@ -3,21 +3,34 @@
         <div>
             <h2>Events that I attended this week!</h2>
             <hr>
+            <event-card v-for="event in events" :key="event.id" :title="event.title" :description="event.description" :date="event.date">
+            </event-card>
         </div>
     </base-card>
 </template>
 <script>
 import axios from 'axios';
+import EventCard from '../UI/EventCard.vue';
 
 export default {
+    components: {
+        EventCard
+    },
+    data(){
+        return {
+            events:[]
+        }
+    },
     created() {
         axios.get('http://localhost:3000/events/')
             .then(response => {
-                console.log(response)
+                const data = response.data;
+                this.events = data;
             })
             .catch(error => {
                 console.log(error)
             });
+            console.log(this.events)
     }
 }
 </script>
